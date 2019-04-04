d7d010df9607d69afa66ca163f5ed1b1fcebbf39
Edu Cortada <cortadaedu@gmail.com>
2019-04-04 09:36:23 +0200

    favicon and text

--- src/pages/Start.js
+++ src/pages/Start.js
@@ -7,7 +7,7 @@ class Start extends Component {
       <React.Fragment>
         <div className="jumbotron">
           <h1 className="display-4">Flight Finder!</h1>
-          <p className="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+          <p className="lead">Find the cheapest flights from spain to some amazing european cities.</p>
           <div className="btns-group">
             <Link to='/signup' className="btn btn-primary">Signup</Link>
             <Link to='/login' className="btn btn-outline-primary">Login</Link>
